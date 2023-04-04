b3bcb4ebb048e1285650a64fe8ae315c20c300f4
IM-SH <sh6609@naver.com>
2023-04-04 21:16:57 +0900

    no message

---
+++ Alone_JS/chapter_2/2-1.js
@@ -0,0 +1,15 @@
+// 혼자 공부하는 자바스크립트 chapter 2-1 (기본 자료형)
+
+//2.
+console.log("#연습문제");
+console.log("\\\\\\\\");
+
+//3.
+console.log("안녕하세요"[1]);
+console.log("안녕하세요"[2]);
+console.log("안녕하세요"[3]);
+console.log("안녕하세요"[4]);
+
+//4.
+console.log(2-2+2/2*2+2);
+console.log(2 + 2-2*2/2*2);
---
+++ Alone_JS/chapter_2/2-2.js
@@ -0,0 +1,23 @@
+//혼자 공부하는 자바 스크립트 chapter2-2 (상수와 변수)
+
+//3-1.
+// const r;
+// r=10;
+
+// console.log(넓이 = `${3.14 * r * r}`);
+// console.log(둘레 = `${2 * 3.14 * r}`);
+
+
+//3-2.
+let r;
+r=10;
+
+console.log(넓이 = `${3.14 * r * r}`);
+console.log(둘레 = `${2 * 3.14 * r}`);
+
+//4.
+const number =10
+console.log(++number)
+console.log(number++)
+console.log(++number)
+console.log(number--)
